5eb6d71883fc9c62a6d806f331fe647ec8e090ee
mercspring <68706508+mercspring@users.noreply.github.com>
2020-10-05 21:07:24 -0700

    completed questions, classes pass all tests, rough draft of inquirer interface

--- app.js
+++ app.js
@@ -1,19 +1,55 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const {employeeTypeQus, beginQus, managerQuestions, engineerQuestions, internQuestions} = require("./lib/Questions")
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { Console } = require("console");
+const { get } = require("http");
+const Questions = require("./lib/Questions");
+
+const employees = [];
 
 
+
+console.log("Welcome Manager. Please enter your details and then you will be asked for details about your team.")
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+inquirer.prompt(beginQus).then((answers) => {
+    if (answers.ready) {
+        inquirer.prompt(managerQuestions).then((answers) => {
+            getEmployeeData(answers)
+        })
+    }
+})
 
+const getEmployeeData = (answers) => {
+    employees.push(answers);
+    inquirer.prompt(employeeTypeQus).then((answers) => {
+        if (answers.employeeType === 'intern') {
+            inquirer.prompt(internQuestions).then((answers) => {
+                employees.push(answers);
+                getEmployeeData()
+            })
+        } else if (answers.employeeType === 'engineer') {
+            inquirer.prompt(engineerQuestions).then((answers) => {
+                employees.push(answers);
+                getEmployeeData()
+            });
+        } else{
+            console.table(employees)
+        }
+
+    })
+}
+// Setup Questions
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
